06d757ed58028bedb4f4bdbaf4d50884c6be6654
Lavitra Sahu <96671738+TechArcanist@users.noreply.github.com>
2024-09-13 22:10:09 +0530

    Create Code.ino

---
+++ Code/Code.ino
@@ -0,0 +1,249 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "xxx";
+const char* password = "xxx";
+
+String chatId = "xxx";
+String BOTtoken = "xxx";
+
+bool sendPhoto = false;
+WiFiClientSecure clientTCP;
+
+UniversalTelegramBot bot(BOTtoken, clientTCP);
+
+#define TRIG_PIN 14
+#define ECHO_PIN 15
+#define FLASH_LED 4
+#define buzzer 13
+
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+String r_msg = "";
+
+const unsigned long BOT_MTBS = 1000;
+unsigned long bot_lasttime;
+
+void handleNewMessages(int numNewMessages);
+String sendPhotoTelegram();
+
+String sendPhotoTelegram() {
+  const char* myDomain = "api.telegram.org";
+  String getAll = "";
+  String getBody = "";
+
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    ESP.restart();
+    return "Camera capture failed";
+  }
+
+  if (clientTCP.connect(myDomain, 443)) {
+    String head = "--xxx\r\nContent-Disposition: form-data; name=\"chat_id\"; \r\n\r\n" + chatId + "\r\n--xxx\r\nContent-Disposition: form-data; name=\"photo\"; filename=\"xxx.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String tail = "\r\n--xxx--\r\n";
+
+    uint16_t imageLen = fb->len;
+    uint16_t extraLen = head.length() + tail.length();
+    uint16_t totalLen = imageLen + extraLen;
+
+    clientTCP.println("POST /bot" + BOTtoken + "/sendPhoto HTTP/1.1");
+    clientTCP.println("Host: " + String(myDomain));
+    clientTCP.println("Content-Length: " + String(totalLen));
+    clientTCP.println("Content-Type: multipart/form-data; boundary=xxx");
+    clientTCP.println();
+    clientTCP.print(head);
+
+    uint8_t *fbBuf = fb->buf;
+    size_t fbLen = fb->len;
+    for (size_t n = 0; n < fbLen; n = n + 1024) {
+      if (n + 1024 < fbLen) {
+        clientTCP.write(fbBuf, 1024);
+        fbBuf += 1024;
+      }
+      else if (fbLen % 1024 > 0) {
+        size_t remainder = fbLen % 1024;
+        clientTCP.write(fbBuf, remainder);
+      }
+    }
+
+    clientTCP.print(tail);
+
+    esp_camera_fb_return(fb);
+
+    int waitTime = 10000;
+    long startTimer = millis();
+    boolean state = false;
+
+    while ((startTimer + waitTime) > millis()) {
+      while (clientTCP.available()) {
+        char c = clientTCP.read();
+        if (c == '\n') {
+          if (getAll.length() == 0) state = true;
+          getAll = "";
+        }
+        else if (c != '\r') {
+          getAll += String(c);
+        }
+        if (state == true) {
+          getBody += String(c);
+        }
+        startTimer = millis();
+      }
+      if (getBody.length() > 0) break;
+    }
+    clientTCP.stop();
+  }
+  else {
+    getBody = "Connected to api.telegram.org failed.";
+  }
+
+  return getBody;
+}
+
+void handleNewMessages(int numNewMessages) {
+  for (int i = 0; i < numNewMessages; i++) {
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != chatId) {
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+
+    String text = bot.messages[i].text;
+
+    String fromName = bot.messages[i].from_name;
+
+    if (text == "/start") {
+      bot.sendMessage(chatId, "Welcome", "Markdown");
+    }
+  }
+}
+
+void configInitCamera() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    ESP.restart();
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);
+    s->set_brightness(s, 1);
+    s->set_saturation(s, -2);
+  }
+  s->set_framesize(s, FRAMESIZE_CIF);
+}
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer,HIGH);
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  Serial.begin(115200);
+  delay(1000);
+
+  pinMode(FLASH_LED, OUTPUT);
+  
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  clientTCP.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println();
+  Serial.print("ESP32-CAM IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Start configuring and initializing the camera...");
+  configInitCamera();
+  Serial.println("Successfully configure and initialize the camera.");
+
+  delay(1000);
+}
+
+void loop() {
+  if (checkDistance() < 5) {
+    Serial.println("Motion detected!");
+    digitalWrite(buzzer,LOW);
+    delay(100);
+    digitalWrite(buzzer,HIGH);
+    sendPhotoTelegram();
+
+  } else {
+    Serial.println("No motion detected.");
+    digitalWrite(buzzer,HIGH);
+  }
+  
+}
+
+float checkDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  float duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration * 0.034 / 2;
+  Serial.println(distance);
+  return distance;
+}
